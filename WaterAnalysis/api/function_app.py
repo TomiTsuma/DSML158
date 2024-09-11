c734744c9fa08699e24d4702b3259e04145c28a1
TomiTsuma <tommytsuma7@gmail.com>
2024-09-11 08:47:06 +0300

    Water verification done

--- WaterAnalysis/api/function_app.py
+++ WaterAnalysis/api/function_app.py
@@ -24,45 +24,65 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             analyses = row['analysis_id']
             for analysis_ in analyses:
                 df_analysis_ = pd.DataFrame(row).T
-                df_analysis_['analysis_name'] = df_analysis_['analysis_id'].apply(lambda x: analysis_dict[x] if x in [ i for i in analysis_dict.keys()] else None)
+                df_analysis_['analysis_name'] = analysis_dict[analysis_]
                 _df = pd.concat([_df, df_analysis_])
         for index,row in _df.iterrows():
-       
             sample_code = row['sample_code']
             analysis = row['analysis_name']
             analysis_id = row['analysis_id']
             if sample_code not in result.keys():
                 result[sample_code] = []
             if analysis not in mahalanobis_thresholds.keys():
-                result[sample_code].append({"sample_code": sample_code,"status":"fail", "message": f"Analysis not in models", "details": f"Analysis: {analysis} is not in the list of defined models" })   
+                result[sample_code].append({"sample_code": sample_code,"status":"warning", "message": f"Analysis not in models", "details": f"Analysis: {analysis} is not in the list of defined models" })   
                 continue     
             scaler = pickle.load(open(f"scalers/{analysis}.pkl","rb"))
             pca = pickle.load(open(f"pca/{analysis}.pkl","rb"))
 
             pca_df = pd.read_csv(f"pca_df/{analysis}.csv",index_col=0)
+            analysis_df = pd.read_csv(f"analysis/{analysis}.csv",index_col=0)
 
             
 
             try :
-                tmp_df = pd.DataFrame(row).T[pca_df.columns]   
-                print(tmp_df)
+                tmp_df = pd.DataFrame(row).T[analysis_df.columns]
+                # tmp_df = tmp_df.dropna() 
             except:
-                result[sample_code].append({"sample_code": sample_code,"status":"fail", "message": f"Missing parameters for analysis_id: {analysis_id}", "details": f"Expected parameters are {','.join(pca_df.columns)} for analysis: {analysis}" })
+                result[sample_code].append({"sample_code": sample_code,"status":"warning", "message": f"Missing parameters for analysis_id: {analysis}", "details": f"Expected parameters are {','.join(pca_df.columns)} for analysis: {analysis}" })
                 continue
             failed_units_comparison = {}    
             for col in pca_df.columns:
                 expected_units = unit_decision.loc[(unit_decision['crop'] == analysis) & (unit_decision['chemical_name'] == col)]
                 print(expected_units[['crop','chemical_name','unit_name']].to_dict())
-                if row[col]['units'] !=   expected_units['unit_name'].values[0] :
+                import math
+                if type(row[col]) != dict and math.isnan(row[col]):
+                    failed_units_comparison[col] = expected_units[['crop','chemical_name','unit_name']].to_dict()
+                elif row[col]['units'] !=   expected_units['unit_name'].values[0] :
                     failed_units_comparison[col] = expected_units[['crop','chemical_name','unit_name']].to_dict()
                 else:
                     row[col] = row[col]['result']
             if len(failed_units_comparison.keys()) > 0:
-                result[sample_code].append({"sample_code": sample_code,"status":"fail", "message": f"Wrong units provided", "details": f"Expected units are {str(failed_units_comparison)} for analysis: {analysis}" })
+                result[sample_code].append({"sample_code": sample_code,"status":"warning", "message": f"Wrong units provided", "details": f"Expected units are {str(failed_units_comparison)} for analysis: {analysis}" })
                 continue
-
-            tmp_df = pd.DataFrame(row).T[pca_df.columns]    
             
+            out_of_bounds_chems = [ ]
+            for col in tmp_df.columns:
+                if col == "sample_code":
+                    continue
+                if col == "ec_salts":
+                    if row[col]['result'] > 105 or row[col]['result'] < 95:
+                        out_of_bounds_chems.append("ec_salts out of bounds. Allowed bounds are 95 - 105")
+                if col == "Charge Balance":
+                    if row[col]['result'] < -1:
+                        out_of_bounds_chems.append("Charge Balance out of bounds. Allowed lower boundary is -10. ")       
+                elif col.lower() == "total suspended solids" and row['analysis_name'].lower() == "total suspended solids":
+                    if row[col] > 1:
+                        out_of_bounds_chems.append("total suspended solids out of bounds. If analysis is total suspended solids is <1 then total suspended solids check should be <1  . ")         
+            if len(out_of_bounds_chems) > 0:
+                result[sample_code].append({"sample_code": sample_code,"status":"warning", "message": f"Out of bounds", "details": f"{'.'.join(out_of_bounds_chems)}" })
+                continue    
+
+            tmp_df = pd.DataFrame(row).T[analysis_df.columns]
+            print(tmp_df.columns)
             # tmp_df = 
             df_scaled = scaler.transform(tmp_df)
             df_pca = pd.DataFrame(pca.transform(df_scaled))
@@ -81,7 +101,7 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
                 result[sample_code].append({"sample_code": sample_code,"status":"fail", "message": "Mahalanobis distance exceeds threshold", "description":f"Mahalanobis distance of {mahalanobis_distance} exceeds threshold of {expected_md} for analysis: {analysis}" })
             else:
                 result[sample_code].append({"sample_code": sample_code,"status":"pass","message": "Mahalanobis distance within threshold", "description":f"Mahalanobis distance of {mahalanobis_distance} is within threshold of {expected_md} for analysis: {analysis}" })
-            
+                    
 
         return func.HttpResponse(json.dumps(result),status_code=200)
     except Exception as e:
